af144f669c655bc36450bbbae99e820f6b63da82
hausken <bj_ha@msn.com>
2023-08-19 08:34:37 +0200

    nix init config

---
+++ bigbother-calamares.nix
@@ -0,0 +1,26 @@
+{ stdenv, lib }:
+
+stdenv.mkDerivation rec {
+  pname = "calamares-nixos-extensions";
+  version = "0.3.12";
+
+  src = ./calamares;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/{lib,share}/calamares
+    cp -r modules $out/lib/calamares/
+    cp -r config/* $out/share/calamares/
+    cp -r branding $out/share/calamares/
+    cp ./modules/nixos/bigbother-config.nix $out/share/calamares/bigbother-config.nix
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Calamares modules for NixOS";
+    homepage = "https://github.com/NixOS/calamares-nixos-extensions";
+    license = with licenses; [ gpl3Plus bsd2 cc-by-40 cc-by-sa-40 cc0 ];
+    maintainers = with maintainers; [ vlinkz ];
+    platforms = platforms.linux;
+  };
+}
---
+++ os.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+# To build this, run:
+# nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=os.nix -I nixpkgs=channel:nixos-23.05
+let
+  localCalamares = import ./bigbother-calamares.nix { inherit (pkgs) stdenv lib; };
+  nixos-version = "23.05";
+in
+{
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    calamares-nixos-extensions = localCalamares;
+  };
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma5.nix>
+    # Provide an initial copy of the NixOS channel so that the user
+    # doesn't need to run "nix-channel --update" first.
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+
+    # Import the main config for the installer
+    (import ./calamares/modules/nixos/bigbother-config.nix { username = "test1"; fullname = "test1full"; nixversion = nixos-version; inherit pkgs; })
+  ];
+
+  services.xserver.layout = "no";
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = nixos-version;
+}
